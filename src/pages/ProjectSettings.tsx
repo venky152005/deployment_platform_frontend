edf3c9af2b17de68be5689840b466038ce419337
venky152005 <venky15.12.2005@gmail.com>
2025-11-23 11:56:21 +0530

    test webhook 13

--- src/pages/ProjectSettings.tsx
+++ src/pages/ProjectSettings.tsx
@@ -5,7 +5,6 @@ import { DashboardLayout } from "@/components/layout/DashboardLayout";
 import { useNavigate, useParams } from "react-router-dom";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Separator } from "@/components/ui/separator";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Badge } from "@/components/ui/badge";
 
